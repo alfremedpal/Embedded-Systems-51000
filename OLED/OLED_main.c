577bec9f4793efda290ef7cdba7c55e599de6fde
Alfredo Medina <35542858+alfremedpal@users.noreply.github.com>
2018-05-24 17:39:02 -0500

    Add files via upload

---
+++ OLED/OLED_main.c
@@ -0,0 +1,73 @@
+/*
+ * File:   OLED_main.c
+ * Author: Alfredo
+ *
+ * Created on 24 de Mayo de 2018, 10:18 AM
+ */
+
+
+#include <xc.h>
+#include "OLED_config.h"
+void configPIC()
+{
+    //All analog pins need to be input
+    //Output: 0; Input: 1
+    
+    INTCONbits.GIE = 0;     //Turn off ALL interrupts
+     
+    ADCON0 = 0b00000000;    //Left-MSB; Right-LSB
+                            //ADC module is off
+    
+    ADCON1 = 0b00001111;    //All ports set to digital I/O
+    
+    TRISA = 0b00000000;     //Set all pins of port A as output
+    TRISB = 0b00000000;     //Set all pins of port B as output
+    TRISC = 0b00000000;     //Set all pins of port C as output
+    TRISD = 0b00000000;     //Set all pins of port D as output
+    TRISE = 0b00000000;     //Set all pins of part E as output
+                            
+   
+    
+    //CONFIGURATION BITS
+    
+    // CONFIG1L
+#pragma config PLLDIV = 5       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+
+// CONFIG1H
+#pragma config FOSC = HSPLL_HS     // Oscillator Selection bits (EC oscillator, CLKO function on RA6 (EC))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+
+// CONFIG2L
+#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
+
+// CONFIG2H
+#pragma config WDT = OFF         // Watchdog Timer Enable bit (WDT enabled)
+
+// CONFIG3H
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config LVP = OFF         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+}
+
+    
+
+void main(void) {
+    
+    configPIC(); 
+    OSCCON = 0x72;
+    
+    OpenI2C();
+    initOLED();
+
+    
+    TRISAbits.RA0 = 0;
+  
+    
+    fb_draw_string_big(44,0,"Linea1");
+    fb_draw_string_big(44,2,"Linea2");
+    fb_show();
+    PORTAbits.RA0 = 1;
+    while(1);
+}
